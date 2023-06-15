c64a57d897dac99e28581a6d504f5f92269cea87
Rijo <benrijo32@gmail.com>
2023-06-15 12:46:44 +0530

    Routing

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,6 +5,8 @@ import Shimmer from "./shimmer";
 const Body = () => {
   // Local State Variable - Super powerful variable
   const [listOfRestaurants, setListOfRestraunt] = useState([]);
+  const [searchedRestaurant, setSearchedRestaurant] = useState([]);
+  const [searchText, setsearchText] = useState("");
 
   // Whenever state variables update, react triggers a reconciliation cycle(re-renders the component)
     useEffect(() => {
@@ -16,20 +18,37 @@ const Body = () => {
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
-
+    console.log(json);
+   
     // Optional Chaining
     setListOfRestraunt(json?.data?.cards[2]?.data?.data?.cards);
+    setSearchedRestaurant(json?.data?.cards[2]?.data?.data?.cards);
+    
 };
 
-if(listOfRestaurants.length === 0){
-    return <Shimmer/>
-}
+// conditional rendering
+// if(listOfRestaurants.length === 0){
+//     return <Shimmer/>
+// }
 
-  return(
+  return listOfRestaurants.length === 0 ? <Shimmer/> : 
+  (
     <div className="body">
-      <div className="filter"></div>
-      <div className="search">
-        <button
+      <div className="filter">
+        <div className="search">
+          <input type="text" className="search-box" value={searchText}
+          onChange={ (e) => {
+            setsearchText(e.target.value);
+          }}  />
+          <button className="search-btn" 
+          onClick={ () =>{
+             const filteredRestaurant = listOfRestaurants.filter(
+              (res) => res.data.name.toLowerCase().includes(searchText.toLowerCase()) 
+             );
+             setSearchedRestaurant(filteredRestaurant);
+          }}>search</button>
+       </div>
+          <button
           className="filter-btn"
           onClick={() => {
             const filteredList = listOfRestaurants.filter(
@@ -39,10 +58,10 @@ if(listOfRestaurants.length === 0){
           }}
           >
           Top Rated Restaurants
-        </button>
-      </div>
-      <div className="res-container">
-        {listOfRestaurants.map((restaurant) => (
+          </button>
+        </div>
+        <div className="res-container">
+         {searchedRestaurant.map((restaurant) => (
           <ResCard key={restaurant.data.id} resData={restaurant} />
         ))}
       </div>
